83bbaf2c55e31ee8aa6941f51e7114c9ceed8d04
Faki74 <fadoul.keitandiaye@outlook.com>
2022-07-29 12:00:06 +0200

    first complete version

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -13,153 +13,209 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  List<RecipeModel> recipes = [];
-
+  List<RecipeModel> recipies = [];
+  String? ingridients;
+  bool _loading = false;
+  String query = "banana";
   TextEditingController textEditingController = TextEditingController();
-  String applicationID = "c418f30c";
-  String applicationKey = "c326f454c4325f7eae2dbe3386c119ae";
-  getRecipies(String query) async {
-    Uri url = Uri.parse(
-        "https://api.edamam.com/search?q=$query&app_id=c418f30c&app_key=c326f454c4325f7eae2dbe3386c119ae");
-    var response = await http.get(url);
-    Map<String, dynamic> JsonData = jsonDecode(response.body);
-    JsonData["hits"].forEach((element) {
-      print(element.toString());
-      RecipeModel recipeModel = RecipeModel();
-      recipeModel = RecipeModel.fromMap(element["recipe"]);
-      recipes.add(recipeModel);
-    });
-    print(recipes.toString());
+
+  String applicationId = "1ea8415d";
+  String applicationKey = "0bc0dbfa7e0d4efef081c98b935bed5f";
+
+  @override
+  void initState() {
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Stack(
-      children: <Widget>[
-        Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          decoration: const BoxDecoration(
-              gradient: LinearGradient(colors: [
-            Color(0xfff03022),
-            Color(0xffa99999),
-          ])),
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(
-              vertical: !kIsWeb
-                  ? Platform.isIOS
-                      ? 60
-                      : 50
-                  : 30,
-              horizontal: 30),
-          child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: kIsWeb
-                      ? MainAxisAlignment.start
-                      : MainAxisAlignment.center,
-                  children: const <Widget>[
-                    Text(
-                      'What to ',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontFamily: 'Overpass'),
-                    ),
-                    Text(
-                      'eat',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.blue,
-                          fontFamily: 'Overpass'),
-                    ),
-                    Text(
-                      '?',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.white,
-                          fontFamily: 'Overpass'),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 60,
-                ),
-                const Text(
-                  "What will we cook chef ?",
-                  style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'Overpass'),
-                ),
-                const Text(
-                  "Tell me what ingredients do you have and i will show you the best recipes ",
-                  style: TextStyle(
-                      fontSize: 15,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w300,
-                      fontFamily: 'OverpassRegular'),
-                ),
-                const SizedBox(
-                  height: 40,
-                ),
-                SizedBox(
-                  width: MediaQuery.of(context).size.width,
-                  child: Row(
-                    children: <Widget>[
-                      Expanded(
-                        child: TextField(
-                          controller: textEditingController,
-                          decoration: InputDecoration(
-                            hintText: "Ingredients ?",
-                            hintStyle: TextStyle(
-                                fontSize: 16,
-                                color: Colors.white.withOpacity(0.5),
-                                fontFamily: 'Overpass'),
-                            enabledBorder: const UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                            focusedBorder: const UnderlineInputBorder(
-                              borderSide: BorderSide(color: Colors.white),
-                            ),
-                          ),
-                        ),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    colors: [Color(0xff213A50), Color(0xff071930)],
+                    begin: FractionalOffset.topRight,
+                    end: FractionalOffset.bottomLeft)),
+          ),
+          SingleChildScrollView(
+            child: Container(
+              padding: EdgeInsets.symmetric(
+                  vertical: !kIsWeb
+                      ? Platform.isIOS
+                          ? 60
+                          : 30
+                      : 30,
+                  horizontal: 24),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Row(
+                    mainAxisAlignment: kIsWeb
+                        ? MainAxisAlignment.start
+                        : MainAxisAlignment.center,
+                    children: const <Widget>[
+                      Text(
+                        "AppGuy",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.white,
+                            fontFamily: 'Overpass'),
                       ),
-                      const SizedBox(
-                        width: 16,
-                      ),
-                      InkWell(
-                        onTap: () async {
-                          if (textEditingController.text.isNotEmpty) {
-                            getRecipies(textEditingController.text);
-                          }
-                        },
-                        child: Container(
-                          child: const Icon(Icons.search, color: Colors.white),
-                        ),
+                      Text(
+                        "Recipes",
+                        style: TextStyle(
+                            fontSize: 18,
+                            color: Colors.blue,
+                            fontFamily: 'Overpass'),
                       )
                     ],
                   ),
-                )
-              ]),
-        )
-      ],
-    ));
+                  const SizedBox(
+                    height: 60,
+                  ),
+                  const Text(
+                    "What will you cook today?",
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Overpass'),
+                  ),
+                  const Text(
+                    "Just Enter Ingredients you have and we will show the best recipe for you",
+                    style: TextStyle(
+                        fontSize: 15,
+                        color: Colors.white,
+                        fontWeight: FontWeight.w300,
+                        fontFamily: 'OverpassRegular'),
+                  ),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: TextField(
+                            controller: textEditingController,
+                            style: const TextStyle(
+                                fontSize: 16,
+                                color: Colors.white,
+                                fontFamily: 'Overpass'),
+                            decoration: InputDecoration(
+                              hintText: "Enter Ingridients",
+                              hintStyle: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.white.withOpacity(0.5),
+                                  fontFamily: 'Overpass'),
+                              enabledBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                              focusedBorder: const UnderlineInputBorder(
+                                borderSide: BorderSide(color: Colors.white),
+                              ),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        InkWell(
+                            onTap: () async {
+                              if (textEditingController.text.isNotEmpty) {
+                                setState(() {
+                                  _loading = true;
+                                });
+                                recipies = [];
+                                String MainUrl =
+                                    "https://api.edamam.com/api/recipes/v2";
+                                String url =
+                                    "$MainUrl?type=public&q=${textEditingController.text}&app_id=1ea8415d&app_key=0bc0dbfa7e0d4efef081c98b935bed5f";
+                                //"https://api.edamam.com/search?q=${textEditingController.text}&app_id=0f21d949&app_key=8bcdd93683d1186ba0555cb95e64ab26";
+                                var response = await http.get(Uri.parse(url));
+                                print(" $response this is response");
+                                Map<String, dynamic> jsonData =
+                                    jsonDecode(response.body);
+                                print("this is json Data $jsonData");
+                                jsonData["hits"].forEach((element) {
+                                  print(element.toString());
+                                  RecipeModel recipeModel = RecipeModel();
+                                  recipeModel =
+                                      RecipeModel.fromMap(element['recipe']);
+                                  recipies.add(recipeModel);
+                                  print(recipeModel.url);
+                                });
+                                setState(() {
+                                  _loading = false;
+                                });
+
+                                print("doing it");
+                              } else {
+                                print("not doing it");
+                              }
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  gradient: const LinearGradient(
+                                      colors: [
+                                        Color(0xffA2834D),
+                                        Color(0xffBC9A5F)
+                                      ],
+                                      begin: FractionalOffset.topRight,
+                                      end: FractionalOffset.bottomLeft)),
+                              padding: const EdgeInsets.all(8),
+                              child: Row(
+                                mainAxisSize: MainAxisSize.min,
+                                children: const <Widget>[
+                                  Icon(Icons.search,
+                                      size: 18, color: Colors.white),
+                                ],
+                              ),
+                            )),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    child: GridView(
+                        gridDelegate:
+                            const SliverGridDelegateWithMaxCrossAxisExtent(
+                                mainAxisSpacing: 10.0,
+                                maxCrossAxisExtent: 200.0),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.vertical,
+                        physics: const ClampingScrollPhysics(),
+                        children: List.generate(recipies.length, (index) {
+                          return GridTile(
+                              child: RecipieTile(
+                            title: recipies[index].label,
+                            imgUrl: recipies[index].image,
+                            desc: recipies[index].source,
+                            url: recipies[index].url,
+                          ));
+                        })),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
 
 class RecipieTile extends StatefulWidget {
   final String title, desc, imgUrl, url;
 
-  const RecipieTile(
-      {required this.title,
-      required this.desc,
-      required this.imgUrl,
-      required this.url});
+  const RecipieTile({this.title, this.desc, this.imgUrl, this.url});
 
   @override
   _RecipieTileState createState() => _RecipieTileState();
@@ -239,6 +295,70 @@ class _RecipieTileState extends State<RecipieTile> {
           ),
         ),
       ],
+    );
+  }
+}
+
+class GradientCard extends StatelessWidget {
+  final Color topColor;
+  final Color bottomColor;
+  final String topColorCode;
+  final String bottomColorCode;
+
+  const GradientCard(
+      {this.topColor,
+      this.bottomColor,
+      this.topColorCode,
+      this.bottomColorCode});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Wrap(
+        children: <Widget>[
+          Container(
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  height: 160,
+                  width: 180,
+                  decoration: BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [topColor, bottomColor],
+                          begin: FractionalOffset.topLeft,
+                          end: FractionalOffset.bottomRight)),
+                ),
+                Container(
+                  width: 180,
+                  alignment: Alignment.bottomLeft,
+                  decoration: const BoxDecoration(
+                      gradient: LinearGradient(
+                          colors: [Colors.white30, Colors.white],
+                          begin: FractionalOffset.centerRight,
+                          end: FractionalOffset.centerLeft)),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                          topColorCode,
+                          style: const TextStyle(
+                              fontSize: 16, color: Colors.black54),
+                        ),
+                        Text(
+                          bottomColorCode,
+                          style: TextStyle(fontSize: 16, color: bottomColor),
+                        )
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
